7892b91cc8c247436cde7cb4337f789c4a8c696c
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-02-25 16:28:55 +0000

    Add React plugin to babel so JSX is converted to ES5

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/env', '@babel/typescript'],
+  presets: ['@babel/env', '@babel/typescript', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-proposal-numeric-separator',
     '@babel/proposal-class-properties',
