162e0de8b9613108e4f447184f5175002815bb5a
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-04-27 15:20:13 +0000

    Add default labelProps

--- src/components/warning-callout/WarningCallout.tsx
+++ src/components/warning-callout/WarningCallout.tsx
@@ -1,16 +1,32 @@
 import React, { HTMLProps } from 'react';
 import classNames from 'classnames';
 
-const WarningCallout: React.FC<HTMLProps<HTMLDivElement>> = ({
+interface WarningCalloutProps extends HTMLProps<HTMLDivElement> {
+  labelProps: HTMLProps<HTMLHeadingElement>;
+}
+
+const WarningCallout: React.FC<WarningCalloutProps> = ({
   className,
   label,
   children,
+  labelProps,
   ...rest
 }) => (
   <div className={classNames('nhsuk-warning-callout', className)} {...rest}>
-    {label ? <h3 className="nhsuk-warning-callout__label">{label}</h3> : null}
+    {label ? (
+      <h3
+        className={classNames('nhsuk-warning-callout__label', labelProps.className)}
+        {...labelProps}
+      >
+        {label}
+      </h3>
+    ) : null}
     {children}
   </div>
 );
+
+WarningCallout.defaultProps = {
+  labelProps: {},
+};
 
 export default WarningCallout;
