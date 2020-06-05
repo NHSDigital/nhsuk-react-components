bad46550caa98eea853058f82eecac13143d5b3f
Thomas Judd-Cooper <Thomas Judd-Cooper>
2020-06-05 09:52:48 +0100

    Allow passing formGroupProps and remove wrapper classes

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,14 +1,13 @@
 import React, { HTMLProps } from 'react';
-import classNames from 'classnames';
 import FormContext from './FormContext';
 
 type FormProps = HTMLProps<HTMLFormElement> & {
   disableErrorFromComponents?: boolean;
 };
 
-const Form: React.FC<FormProps> = ({ className, disableErrorFromComponents, ...rest }) => (
+const Form: React.FC<FormProps> = ({ disableErrorFromComponents, ...rest }) => (
   <FormContext.Provider value={{ disableErrorFromComponents: Boolean(disableErrorFromComponents) }}>
-    <form className={classNames('nhsuk-form-group--wrapper', className)} {...rest} />
+    <form {...rest} />
   </FormContext.Provider>
 );
 
