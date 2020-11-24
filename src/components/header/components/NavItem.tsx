123c78e7e2ed08d45774f851c8566e6c15331d83
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2020-11-24 15:59:38 +0000

    added ability to change component/element for nav item

--- src/components/header/components/NavItem.tsx
+++ src/components/header/components/NavItem.tsx
@@ -3,22 +3,32 @@ import classNames from 'classnames';
 import { ChevronRight as ChevronRightIcon } from '../../icons';
 
 export interface NavItemProps extends HTMLProps<HTMLAnchorElement> {
+  asElement?: React.ElementType;
+  to?: string;
   mobileOnly?: boolean;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ mobileOnly, className, children, ...rest }) => (
-  <li
-    className={classNames(
-      'nhsuk-header__navigation-item',
-      { 'nhsuk-header__navigation-item--for-mobile': mobileOnly },
-      className,
-    )}
-  >
-    <a className="nhsuk-header__navigation-link" {...rest}>
-      {children}
-      <ChevronRightIcon />
-    </a>
-  </li>
-);
+const NavItem: React.FC<NavItemProps> = ({
+  mobileOnly,
+  className,
+  children,
+  asElement: Component = 'a',
+  ...rest
+}) => {
+  return (
+    <li
+      className={classNames(
+        'nhsuk-header__navigation-item',
+        { 'nhsuk-header__navigation-item--for-mobile': mobileOnly },
+        className,
+      )}
+    >
+      <Component className="nhsuk-header__navigation-link" {...rest}>
+        {children}
+        <ChevronRightIcon />
+      </Component>
+    </li>
+  );
+};
 
 export default NavItem;
