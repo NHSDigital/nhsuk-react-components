3895d428ff212911632a534b486bd29a155b1b26
Ramya Subramanian <ramya.subramanian@nhs.net>
2021-02-18 15:10:47 +0000

    #71 exposing form group component to the consumers, (error, hint, label, input, select, textarea)

--- src/components/hint/Hint.tsx
+++ src/components/hint/Hint.tsx
@@ -1,10 +1,25 @@
-import React, { HTMLProps } from 'react';
+import React, { HTMLProps, useContext, useEffect } from 'react';
+
+import FormGroupContext from '../formgroup/FormGroupContext';
 import classNames from 'classnames';
 
 export type HintProps = HTMLProps<HTMLDivElement>;
 
-const Hint: React.FC<HintProps> = ({ className, ...rest }) => (
-  <div className={classNames('nhsuk-hint', className)} {...rest} />
-);
+const Hint: React.FC<HintProps> = props => {
+  const { isInFormGroup, setInputID } = useContext(FormGroupContext);
+
+  useEffect(() => {
+    if (isInFormGroup && props.id) {
+      setInputID(props.id);
+      return () => {
+        setInputID(undefined);
+      };
+    }
+    return () => {};
+  }, [isInFormGroup, props.id]);
+
+  const { className, ...rest } = props;
+  return <div className={classNames('nhsuk-hint', className)} {...rest} />;
+};
 
 export default Hint;
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,8 +1,9 @@
-import React, { HTMLProps } from 'react';
+import React, { HTMLProps, useContext, useEffect } from 'react';
 import classNames from 'classnames';
 import FormGroup from '../../util/FormGroup';
 import { InputWidth } from '../../util/types/NHSUKTypes';
 import { FormElementProps } from '../../util/types/FormTypes';
+import FormGroupContext from '../formgroup/FormGroupContext';
 
 interface InputProps extends HTMLProps<HTMLInputElement>, FormElementProps {
   inputRef?: (inputRef: HTMLInputElement | null) => any;
@@ -10,23 +11,43 @@ interface InputProps extends HTMLProps<HTMLInputElement>, FormElementProps {
   disableErrorLine?: boolean;
 }
 
-const Input: React.FC<InputProps> = props => (
-  <FormGroup<InputProps> {...props} inputType="input">
-    {({ width, className, error, inputRef, ...rest }) => (
-      <input
-        className={classNames(
-          'nhsuk-input',
-          { [`nhsuk-input--width-${width}`]: width },
-          { 'nhsuk-input--error': error },
-          className,
-        )}
-        ref={inputRef}
-        {...rest}
-      />
-    )}
-  </FormGroup>
-);
+const Input: React.FC<InputProps> = props => {
+  const { isInFormGroup, setInputID } = useContext(FormGroupContext);
+  useEffect(() => {
+    if (isInFormGroup && props.id) {
+      setInputID(props.id);
+      return () => {
+        setInputID(undefined);
+      };
+    }
+    return () => {};
+  }, [isInFormGroup, props.id]);
 
+  if (isInFormGroup) {
+    return <InputElement {...props} />;
+  }
+  return (
+    <FormGroup<InputProps> {...props} inputType="input">
+      {renderProps => <InputElement {...renderProps} />}
+    </FormGroup>
+  );
+};
+
+const InputElement: React.FC<InputProps> = props => {
+  const { width, className, error, inputRef, hint, label, ...rest } = props;
+  return (
+    <input
+      className={classNames(
+        'nhsuk-input',
+        { [`nhsuk-input--width-${width}`]: width },
+        { 'nhsuk-input--error': error },
+        className,
+      )}
+      ref={inputRef}
+      {...rest}
+    />
+  );
+};
 Input.defaultProps = {
   type: 'text',
 };
