9065d4480334433fd4d06facecce16256b62e90a
Arup Banerjee <arup@alanaamy.net>
2021-04-27 09:45:05 +0100

    added more test for Select component

--- src/components/select/__tests__/Select.test.tsx
+++ src/components/select/__tests__/Select.test.tsx
@@ -17,10 +17,20 @@ describe(`Select`, () => {
           <Select onClick={handleClick} selectRef={ref} />
         );
     };
-    it(`should do nothing if ref does not exist`, () => {
+
+    it(`should do nothing if ref does not Exist`, () => {
         const useRefSpy = jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: null });
         const component = shallow(<SelectComp />);
         component.find('Select').simulate('click');
         expect(useRefSpy).toBeCalledWith(null);
+    });
+
+    it('should handle DOM events where ref Exists', () => {
+        const useRefSpy = jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: document.createElement('select') });
+        const mock = jest.fn();
+        const component = shallow(<SelectComp onHandle={mock} />);
+        component.find('Select').simulate('click');
+        expect(useRefSpy).toBeCalledWith(null);
+        expect(mock).toBeCalledTimes(1);
     });
 })
