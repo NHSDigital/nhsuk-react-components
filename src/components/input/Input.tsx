4a79a08a7f3d4b3959546a0d13bec6176b9f1117
Ramya Subramanian <ramya.subramanian@nhs.net>
2021-05-07 14:36:16 +0100

    Added Form Group

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,8 +1,12 @@
-import React, { HTMLProps } from 'react';
-import classNames from 'classnames';
-import FormGroup from '../../util/FormGroup';
-import { InputWidth } from '../../util/types/NHSUKTypes';
+import React, { HTMLProps, useContext, useEffect } from 'react';
+
 import { FormElementProps } from '../../util/types/FormTypes';
+import FormGroup from '../../util/FormGroup';
+// eslint-disable-next-line import/no-named-as-default
+import FormGroupContext from '../formgroup/FormGroupContext';
+import { InputWidth } from '../../util/types/NHSUKTypes';
+// eslint-disable-next-line
+import classNames from 'classnames';
 
 interface InputProps extends HTMLProps<HTMLInputElement>, FormElementProps {
   inputRef?: (inputRef: HTMLInputElement | null) => any;
@@ -10,23 +14,43 @@ interface InputProps extends HTMLProps<HTMLInputElement>, FormElementProps {
   disableErrorLine?: boolean;
 }
 
-const Input: React.FC<InputProps> = props => (
-  <FormGroup<InputProps> {...props} inputType="input">
-    {({ width, className, error, inputRef, ...rest }) => (
-      <input
-        className={classNames(
-          'nhsuk-input',
-          { [`nhsuk-input--width-${width}`]: width },
-          { 'nhsuk-input--error': error },
-          className,
-        )}
-        ref={inputRef}
-        {...rest}
-      />
-    )}
-  </FormGroup>
-);
+const Input: React.FC<InputProps> = props => {
+  const { isInFormGroup, setInputID } = useContext(FormGroupContext);
+  useEffect(() => {
+    if (isInFormGroup && props.id) {
+      setInputID(props.id);
+      return () => {
+        setInputID(undefined);
+      };
+    }
+    return () => {};
+  }, [isInFormGroup, props.id]);
 
+  if (isInFormGroup) {
+    return <InputElement {...props} />;
+  }
+  return (
+    <FormGroup<InputProps> {...props} inputType="input">
+      {renderProps => <InputElement {...renderProps} />}
+    </FormGroup>
+  );
+};
+
+const InputElement: React.FC<InputProps> = props => {
+  const { width, className, error, inputRef, hint, label, ...rest } = props;
+  return (
+    <input
+      className={classNames(
+        'nhsuk-input',
+        { [`nhsuk-input--width-${width}`]: width },
+        { 'nhsuk-input--error': error },
+        className,
+      )}
+      ref={inputRef}
+      {...rest}
+    />
+  );
+};
 Input.defaultProps = {
   type: 'text',
 };
--- src/components/textarea/Textarea.tsx
+++ src/components/textarea/Textarea.tsx
@@ -1,19 +1,43 @@
-import React, { HTMLProps } from 'react';
+import React, { HTMLProps, useContext, useEffect } from 'react';
 import classNames from 'classnames';
 import { FormElementProps } from '../../util/types/FormTypes';
 import FormGroup from '../../util/FormGroup';
+import { FormGroupContext } from '../formgroup/FormGroupContext';
 
 type TextareaProps = HTMLProps<HTMLTextAreaElement> & FormElementProps;
 
-const Textarea: React.FC<TextareaProps> = props => (
-  <FormGroup<TextareaProps> inputType="textarea" {...props}>
-    {({ className, error, ...rest }) => (
+const Textarea: React.FC<TextareaProps> = props => {
+  const { isInFormGroup, setInputID } = useContext(FormGroupContext);
+  useEffect(() => {
+    if (isInFormGroup && props.id) {
+      setInputID(props.id);
+      return () => {
+        setInputID(undefined);
+      };
+    }
+    return () => {};
+  }, [isInFormGroup, props.id]);
+
+  if (isInFormGroup) {
+    const { className, error, ...rest } = props;
+    return (
       <textarea
         className={classNames('nhsuk-textarea', { 'nhsuk-textarea--error': error }, className)}
         {...rest}
       />
-    )}
-  </FormGroup>
-);
+    );
+  }
+
+  return (
+    <FormGroup<TextareaProps> inputType="textarea" {...props}>
+      {({ className, error, ...rest }) => (
+        <textarea
+          className={classNames('nhsuk-textarea', { 'nhsuk-textarea--error': error }, className)}
+          {...rest}
+        />
+      )}
+    </FormGroup>
+  );
+};
 
 export default Textarea;
