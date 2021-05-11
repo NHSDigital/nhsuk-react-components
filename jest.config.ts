4b17208158cc857c2d96e53310e7dcac6baf540a
Thomas Judd-Cooper <me@tomjuddcooper.co.uk>
2021-05-11 19:26:24 +0100

    Collect full coverage

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   rootDir: 'src/',
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupFiles: ['<rootDir>/setupTests.ts'],
+  collectCoverageFrom: ['<rootDir>/**/*.{ts,tsx}'],
 };
 
 export default config;
