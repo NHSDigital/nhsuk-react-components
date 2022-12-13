34f900f1dfb42f50e8b5a242431ca5e95b230f61
Filipe Boldo <filipeboldo@gmail.com>
2022-12-13 16:46:31 +0000

    ✨ feat: add textareaRef prop (#123)

--- src/components/textarea/Textarea.tsx
+++ src/components/textarea/Textarea.tsx
@@ -1,15 +1,17 @@
-import React, { HTMLProps } from 'react';
+import React, { HTMLProps, MutableRefObject } from 'react';
 import classNames from 'classnames';
 import { FormElementProps } from '../../util/types/FormTypes';
 import FormGroup from '../../util/FormGroup';
 
-type TextareaProps = HTMLProps<HTMLTextAreaElement> & FormElementProps;
+type TextareaProps = HTMLProps<HTMLTextAreaElement> &
+  FormElementProps & { textareaRef?: MutableRefObject<HTMLTextAreaElement | null> };
 
 const Textarea: React.FC<TextareaProps> = (props) => (
   <FormGroup<TextareaProps> inputType="textarea" {...props}>
-    {({ className, error, ...rest }) => (
+    {({ className, error, textareaRef, ...rest }) => (
       <textarea
         className={classNames('nhsuk-textarea', { 'nhsuk-textarea--error': error }, className)}
+        ref={textareaRef}
         {...rest}
       />
     )}
