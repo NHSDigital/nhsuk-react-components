e813d42559b6f91abd5bf1be990d7e881dc5b890
kai <kaispencer98@gmail.com>
2023-03-21 23:16:59 +0000

    Provide custom ordering to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
-import "./storybook.scss";
+import './storybook.scss';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  options: {
+    storySort: { order: ['Components', 'Patterns', 'FormBehaviour', 'Deprecated'] },
+  },
 };
