9c4c32df0a9ab4ae2090ac4e7e5c39132e83ff84
Kai Spencer <51139521+KaiSpencer@users.noreply.github.com>
2023-04-12 14:47:05 +0100

    Remove footer three column variant (#151)
    * Remove footer three column prop and test
    
    * Remove story with columns
    
    * Update branch with latest changes

--- stories/Components/Footer.stories.tsx
+++ stories/Components/Footer.stories.tsx
@@ -26,28 +26,3 @@ export const Standard: Story = {
     </>
   ),
 };
-
-export const WithThreeColumns: Story = {
-  render: () => (
-    <>
-      <div id="restOfThePage" style={{ height: '60vh' }} />
-      <Footer>
-        <Footer.List columns>
-          <Footer.ListItem href="https://www.nhs.uk/nhs-sites/">NHS sites</Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/about-us/">About us</Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/contact-us/">Contact us</Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/personalisation/login.aspx">
-            Profile editor login
-          </Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/about-us/sitemap/">Sitemap</Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/accessibility/">Accessibility</Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/our-policies/">Our policies</Footer.ListItem>
-          <Footer.ListItem href="https://www.nhs.uk/our-policies/cookies-policy/">
-            Cookies
-          </Footer.ListItem>
-        </Footer.List>
-        <Footer.Copyright>&copy; Crown copyright</Footer.Copyright>
-      </Footer>
-    </>
-  ),
-};
