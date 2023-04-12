e5118fb2c946d32977cc4f00480117e30e1d041d
kai <kaispencer98@gmail.com>
2023-04-12 17:48:46 +0100

    add prefixText prop and tests for DoDontList

--- src/components/do-dont-list/__tests__/DoDontList.test.tsx
+++ src/components/do-dont-list/__tests__/DoDontList.test.tsx
@@ -64,7 +64,7 @@ describe('DoDontList', () => {
       dontList.unmount();
     });
 
-    it("dont item includes 'do not'", () => {
+    it("dont item includes 'do not' by default", () => {
       const dontList = mount(
         <DoDontList listType="dont">
           <DoDontList.Item>do something bad</DoDontList.Item>
@@ -73,5 +73,40 @@ describe('DoDontList', () => {
       expect(dontList.find('.nhsuk-list--cross').text()).toEqual('do not do something bad');
       dontList.unmount();
     });
+
+    it("items render custom prefix text", () => {
+      const doList = mount(
+        <DoDontList listType="do">
+          <DoDontList.Item prefixText="do ">something good 1</DoDontList.Item>
+          <DoDontList.Item>something good 2</DoDontList.Item>
+          <DoDontList.Item prefixText={<span>also do </span>}>something good 3</DoDontList.Item>
+          <DoDontList.Item prefixText={undefined}>something good 4</DoDontList.Item>
+          <DoDontList.Item prefixText={null}>something good 5</DoDontList.Item>
+        </DoDontList>,
+      );
+      const dontList = mount(
+        <DoDontList listType="dont">
+          <DoDontList.Item prefixText="do not ">do something bad 1</DoDontList.Item>
+          <DoDontList.Item>do something bad 2</DoDontList.Item>
+          <DoDontList.Item prefixText={<span>don&apos;t do </span>}>something bad 3</DoDontList.Item>
+          <DoDontList.Item prefixText={undefined}>something bad 4</DoDontList.Item>
+          <DoDontList.Item prefixText={null}>something bad 5</DoDontList.Item>
+        </DoDontList>,
+      );
+      expect(doList.find('.nhsuk-list--tick').childAt(0).text()).toBe('do something good 1');
+      expect(doList.find('.nhsuk-list--tick').childAt(1).text()).toBe('something good 2');
+      expect(doList.find('.nhsuk-list--tick').childAt(2).text()).toBe('also do something good 3');
+      expect(doList.find('.nhsuk-list--tick').childAt(3).text()).toBe('something good 4');
+      expect(doList.find('.nhsuk-list--tick').childAt(4).text()).toBe('something good 5');
+
+      expect(dontList.find('.nhsuk-list--cross').childAt(0).text()).toBe('do not do something bad 1');
+      expect(dontList.find('.nhsuk-list--cross').childAt(1).text()).toBe('do not do something bad 2');
+      expect(dontList.find('.nhsuk-list--cross').childAt(2).text()).toBe('don\'t do something bad 3');
+      expect(dontList.find('.nhsuk-list--cross').childAt(3).text()).toBe('do not something bad 4');
+      expect(dontList.find('.nhsuk-list--cross').childAt(4).text()).toBe('something bad 5');
+
+      doList.unmount();
+      dontList.unmount();
+    })
   });
 });
