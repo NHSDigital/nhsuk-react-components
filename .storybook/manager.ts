603cfbf5eceaef9ba86d0ee2a20e51f28db86fd1
Ed Horsford <mail@edwardhorsford.com>
2024-06-11 12:29:01 +0100

    Use sentence case for sidebar

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,16 @@
 import { addons } from '@storybook/manager-api';
 import nhsTheme from './theme';
+import { startCase, upperFirst } from "lodash";
+
+const sentenceCase = string => {
+  if (typeof string !== 'string') return ''
+  return upperFirst(startCase(string).toLowerCase())
+}
 
 addons.setConfig({
-  theme: nhsTheme,
+  sidebar: {
+    renderLabel: ({ name, type }) =>
+      sentenceCase(name),
+  },
+  theme: nhsTheme
 });
