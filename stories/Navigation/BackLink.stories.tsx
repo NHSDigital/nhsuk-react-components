0c2c115f57f1a0ca185ae315f19def5f41e95273
Ed Horsford <mail@edwardhorsford.com>
2024-06-11 13:36:40 +0100

    Update BackLink docs (#234)

--- stories/Navigation/BackLink.stories.tsx
+++ stories/Navigation/BackLink.stories.tsx
@@ -4,15 +4,16 @@ import { Meta, StoryObj } from '@storybook/react';
 const meta: Meta<typeof BackLink> = {
   title: 'Navigation/BackLink',
   component: BackLink,
-  args: { children: 'Link', href: '/', asElement: 'a' },
+  args: { children: 'Go back', href: '/', asElement: 'a' },
 };
 export default meta;
 type Story = StoryObj<typeof BackLink>;
 
 export const StandardLink: Story = {};
-export const OpenInNewTabLink: Story = {
+
+export const BackLinkAsAButton: Story = {
   args: {
-    target: '_blank',
-    rel: 'noopener noreferrer',
+    asElement: 'button',
+    href: false
   },
 };
