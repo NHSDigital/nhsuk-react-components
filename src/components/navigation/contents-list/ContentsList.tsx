de83ba3c7d8364c3038fa250a2300c423ddf0f5f
Colin Rotherham <work@colinr.com>
2025-09-08 12:01:34 +0100

    Fix contents list missing ARIA attributes

--- src/components/navigation/contents-list/ContentsList.tsx
+++ src/components/navigation/contents-list/ContentsList.tsx
@@ -6,7 +6,10 @@ interface ContentsListItemProps extends HTMLProps<HTMLAnchorElement> {
 }
 
 const ContentsListItem: FC<ContentsListItemProps> = ({ className, current, ...rest }) => (
-  <li className={classNames('nhsuk-contents-list__item', className)}>
+  <li
+    className={classNames('nhsuk-contents-list__item', className)}
+    aria-current={current ? 'page' : undefined}
+  >
     {current ? (
       <span className="nhsuk-contents-list__current" {...rest} />
     ) : (
@@ -27,10 +30,16 @@ const ContentsListComponent: ContentsListComponent = ({
   className,
   children,
   role = 'navigation',
+  'aria-label': ariaLabel = 'Pages in this guide',
   visuallyHiddenText = 'Contents',
   ...rest
 }) => (
-  <nav className={classNames('nhsuk-contents-list', className)} role={role} {...rest}>
+  <nav
+    className={classNames('nhsuk-contents-list', className)}
+    role={role}
+    aria-label={ariaLabel}
+    {...rest}
+  >
     <h2 className="nhsuk-u-visually-hidden">{visuallyHiddenText}</h2>
     <ol className="nhsuk-contents-list__list">{children}</ol>
   </nav>
