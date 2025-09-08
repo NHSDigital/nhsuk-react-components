ca52faeaea5234375801a93fd0af55058a41ba47
Colin Rotherham <work@colinr.com>
2025-09-08 11:27:56 +0100

    Remove support for inset text optional visually hidden text

--- src/components/content-presentation/inset-text/InsetText.tsx
+++ src/components/content-presentation/inset-text/InsetText.tsx
@@ -1,20 +1,9 @@
 import React, { FC, HTMLProps } from 'react';
 import classNames from 'classnames';
 
-interface InsetTextProps extends HTMLProps<HTMLDivElement> {
-  visuallyHiddenText?: string | false;
-}
-
-const InsetTextComponent: FC<InsetTextProps> = ({
-  className,
-  children,
-  visuallyHiddenText = 'Information: ',
-  ...rest
-}) => (
+const InsetTextComponent: FC<HTMLProps<HTMLDivElement>> = ({ className, children, ...rest }) => (
   <div className={classNames('nhsuk-inset-text', className)} {...rest}>
-    {visuallyHiddenText ? (
-      <span className="nhsuk-u-visually-hidden">{visuallyHiddenText}</span>
-    ) : null}
+    <span className="nhsuk-u-visually-hidden">Information: </span>
     {children}
   </div>
 );
--- src/components/content-presentation/inset-text/__tests__/InsetText.test.tsx
+++ src/components/content-presentation/inset-text/__tests__/InsetText.test.tsx
@@ -15,18 +15,6 @@ describe('InsetText', () => {
     expect(container.querySelector('.nhsuk-u-visually-hidden')?.textContent).toBe('Information: ');
   });
 
-  it('has disabled visually hidden text', () => {
-    const { container } = render(<InsetText visuallyHiddenText={false} />);
-
-    expect(container.querySelector('.nhsuk-u-visually-hidden')).toBeFalsy();
-  });
-
-  it('has custom visually hidden text', () => {
-    const { container } = render(<InsetText visuallyHiddenText="Custom" />);
-
-    expect(container.querySelector('.nhsuk-u-visually-hidden')?.textContent).toBe('Custom');
-  });
-
   it('renders children', () => {
     const { container } = render(<InsetText>Child</InsetText>);
 
