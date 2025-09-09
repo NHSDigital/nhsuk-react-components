6b99280f3122e824690774c4a24c5f96e4b47e72
ricksmith130 <richard.smith130@nhs.net>
2025-09-09 09:35:17 +0100

    adjusted eslint formatting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,36 +3,35 @@ import { mergeConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.@(ts|tsx)', '../stories/**/*.mdx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+	stories: ['../stories/**/*.stories.@(ts|tsx)', '../stories/**/*.mdx'],
+	addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
 
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+	},
 
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-  },
-
-  viteFinal(config) {
-    return mergeConfig(config, {
-      plugins: [tsConfigPaths()],
-      css: {
-        preprocessorOptions: {
-          scss: {
-            // Hide deprecation noise coming from dependencies like nhsuk-frontend
-            quietDeps: true,
-            // Specifically silence the Dart Sass "misplaced-rest" deprecation
-            // (Type cast to any in case your TS types don't include this yet)
-            silenceDeprecations: ['misplaced-rest'],
-            // Optional: ensure node_modules is in the Sass load path
-            // includePaths: ['node_modules'],
-          } as any,
-        },
-      },
-    });
-  }
+	viteFinal(config) {
+		return mergeConfig(config, {
+		plugins: [tsConfigPaths()],
+		css: {
+			preprocessorOptions: {
+			scss: {
+				// Hide deprecation noise coming from dependencies like nhsuk-frontend
+				quietDeps: true,
+				// Specifically silence the Dart Sass "misplaced-rest" deprecation
+				// (Type cast to any in case your TS types don't include this yet)
+				silenceDeprecations: ['misplaced-rest'],
+				// Optional: ensure node_modules is in the Sass load path
+				// includePaths: ['node_modules'],
+			} as any,
+			},
+		},
+		});
+	}
 };
 
 export default config;
