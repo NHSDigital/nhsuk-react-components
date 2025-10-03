67dcf2a3998c1790e52b5b030c341b8a010e2a99
Colin Rotherham <work@colinr.com>
2025-10-03 15:54:48 +0100

    Make sure source maps include original sources

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -26,6 +26,7 @@ export default defineConfig(
           preserveModules: true,
           preserveModulesRoot: 'src',
           sourcemap: true,
+          sourcemapExcludeSources: true,
         },
       ],
       plugins: [
