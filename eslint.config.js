933e48d0e19316be339f09082ebc1a4096bc97af
Colin Rotherham <work@colinr.com>
2025-10-03 16:48:26 +0100

    Remove ESLint rules we should fix instead

--- eslint.config.js
+++ eslint.config.js
@@ -58,11 +58,6 @@ export default defineConfig([
     },
     rules: {
       'import/no-unresolved': 'off',
-      'react/prop-types': 0,
-      'jsx-a11y/anchor-has-content': 0,
-      'jsx-a11y/alt-text': 0,
-      'jsx-a11y/heading-has-content': 0,
-      'react-hooks/exhaustive-deps': 0,
     },
     settings: {
       react: {
