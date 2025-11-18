bc47ba8d4915efe6b0917915000779b22734a6a0
Colin Rotherham <work@colinr.com>
2025-11-18 11:55:10 +0000

    Add stories for summary list row `noBorder` and `noActions`

--- stories/Content Presentation/SummaryList.stories.tsx
+++ stories/Content Presentation/SummaryList.stories.tsx
@@ -68,6 +68,53 @@ export const Standard: Story = {
   ),
 };
 
+export const SummaryListWithoutActionsOnLastRow: Story = {
+  render: (args) => (
+    <SummaryList {...args}>
+      <SummaryList.Row>
+        <SummaryList.Key>Name</SummaryList.Key>
+        <SummaryList.Value>Karen Francis</SummaryList.Value>
+        <SummaryList.Actions>
+          <SummaryList.Action href="#" visuallyHiddenText="name">
+            Change
+          </SummaryList.Action>
+        </SummaryList.Actions>
+      </SummaryList.Row>
+      <SummaryList.Row>
+        <SummaryList.Key>Date of birth</SummaryList.Key>
+        <SummaryList.Value>15 March 1984</SummaryList.Value>
+        <SummaryList.Actions>
+          <SummaryList.Action href="#" visuallyHiddenText="date of birth">
+            Change
+          </SummaryList.Action>
+        </SummaryList.Actions>
+      </SummaryList.Row>
+      <SummaryList.Row>
+        <SummaryList.Key>Contact information</SummaryList.Key>
+        <SummaryList.Value>
+          73 Roman Rd
+          <br />
+          Leeds
+          <br />
+          LS2 5ZN
+        </SummaryList.Value>
+        <SummaryList.Actions>
+          <SummaryList.Action href="#" visuallyHiddenText="contact information">
+            Change
+          </SummaryList.Action>
+        </SummaryList.Actions>
+      </SummaryList.Row>
+      <SummaryList.Row noActions>
+        <SummaryList.Key>Contact details</SummaryList.Key>
+        <SummaryList.Value>
+          <BodyText>07700 900457</BodyText>
+          <BodyText>sarah.phillips@example.com</BodyText>
+        </SummaryList.Value>
+      </SummaryList.Row>
+    </SummaryList>
+  ),
+};
+
 export const SummaryListWithoutActions: Story = {
   render: (args) => (
     <SummaryList {...args}>
@@ -90,6 +137,38 @@ export const SummaryListWithoutActions: Story = {
         </SummaryList.Value>
       </SummaryList.Row>
       <SummaryList.Row>
+        <SummaryList.Key>Contact details</SummaryList.Key>
+        <SummaryList.Value>
+          <BodyText>07700 900457</BodyText>
+          <BodyText>sarah.phillips@example.com</BodyText>
+        </SummaryList.Value>
+      </SummaryList.Row>
+    </SummaryList>
+  ),
+};
+
+export const SummaryListWithoutBorderOnLastRow: Story = {
+  render: (args) => (
+    <SummaryList {...args}>
+      <SummaryList.Row>
+        <SummaryList.Key>Name</SummaryList.Key>
+        <SummaryList.Value>Karen Francis</SummaryList.Value>
+      </SummaryList.Row>
+      <SummaryList.Row>
+        <SummaryList.Key>Date of birth</SummaryList.Key>
+        <SummaryList.Value>15 March 1984</SummaryList.Value>
+      </SummaryList.Row>
+      <SummaryList.Row>
+        <SummaryList.Key>Contact information</SummaryList.Key>
+        <SummaryList.Value>
+          73 Roman Rd
+          <br />
+          Leeds
+          <br />
+          LS2 5ZN
+        </SummaryList.Value>
+      </SummaryList.Row>
+      <SummaryList.Row noBorder>
         <SummaryList.Key>Contact details</SummaryList.Key>
         <SummaryList.Value>
           <BodyText>07700 900457</BodyText>
