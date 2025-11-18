84ca837d7955cc4a39121d66e02fc780be50df2e
Colin Rotherham <work@colinr.com>
2025-11-18 11:12:05 +0000

    Add missing summary list row `noActions` prop

--- src/components/content-presentation/summary-list/components/SummaryListRow.tsx
+++ src/components/content-presentation/summary-list/components/SummaryListRow.tsx
@@ -2,13 +2,20 @@ import classNames from 'classnames';
 import { type ComponentPropsWithoutRef, type FC } from 'react';
 
 export interface SummaryListRowProps extends ComponentPropsWithoutRef<'div'> {
+  noActions?: boolean;
   noBorder?: boolean;
 }
 
-export const SummaryListRow: FC<SummaryListRowProps> = ({ className, noBorder, ...rest }) => (
+export const SummaryListRow: FC<SummaryListRowProps> = ({
+  className,
+  noActions,
+  noBorder,
+  ...rest
+}) => (
   <div
     className={classNames(
       'nhsuk-summary-list__row',
+      { 'nhsuk-summary-list__row--no-actions': noActions },
       { 'nhsuk-summary-list__row--no-border': noBorder },
       className,
     )}
